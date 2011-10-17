b98f7b0b21c79d8a37f4e53a26fb46ecd83be3b9
Nathan Broadbent <nathan.f77@gmail.com>
2011-10-18 00:52:48 +0800

    Docs

--- install.sh
+++ install.sh
@@ -5,14 +5,17 @@ exec_string='[[ -s "$HOME/.scm_breeze/scm_breeze.sh" ]] && . "$HOME/.scm_breeze/
 for rc in bashrc zshrc; do
   if [[ -s "$HOME/.$rc" ]] && ! grep -q "$exec_string" "$HOME/.$rc"; then
     echo -e "\n$exec_string" >> "$HOME/.$rc"
+    echo "== Added SCM Breeze to '~/.$rc'"
   fi
 done
 
 
-# Set up
+# Set up ~/*.scmbrc files
+# ---------------------------------------------------------------------------------------------
+# Git
 if ! [[ -s "$HOME/.git.scmbrc" ]]; then
   cp "$HOME/.scm_breeze/git.scmbrc.example" "$HOME/.git.scmbrc"
   echo "== 'git.scmbrc.example' has been copied to '~/.git.scmbrc'.
-   Please edit this file to change SCM Breeze settings, and Git aliases & keyboard shortcuts."
+   Please edit this file to change SCM Breeze settings, Git aliases, and Git keyboard bindings."
 fi
 
