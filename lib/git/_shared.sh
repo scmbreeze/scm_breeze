a6bc784cc5f26918b706c8e2a25719569ff2230a
Nathan Broadbent <nathan.f77@gmail.com>
2011-10-19 20:12:29 +0800

    removed dup file

--- lib/git/_shared.sh
+++
@@ -1,8 +0,0 @@
-# Detect shell
-if [ -n "${ZSH_VERSION:-}" ]; then shell="zsh"; else shell="bash"; fi
-# Detect whether zsh 'shwordsplit' option is on by default.
-if [[ $shell == "zsh" ]]; then zsh_shwordsplit=$((setopt | grep -q shwordsplit) && echo "true"); fi
-# Switch on/off shwordsplit for functions that require it.
-zsh_compat(){ if [[ $shell == "zsh" && -z $zsh_shwordsplit ]]; then setopt shwordsplit; fi; }
-zsh_reset(){  if [[ $shell == "zsh" && -z $zsh_shwordsplit ]]; then unsetopt shwordsplit; fi; }
-
