51d3ad0fb840f48ab60d772f3e935d2ff1eb3913
Nathan Broadbent <nathan.f77@gmail.com>
2011-10-20 22:11:15 +0800

    Added shebang to install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
+#!/bin/bash
 # This loads SCM Breeze into the shell session.
 exec_string='[[ -s "$HOME/.scm_breeze/scm_breeze.sh" ]] && . "$HOME/.scm_breeze/scm_breeze.sh"'
 
 # Add line to bashrc and zshrc if not already present.
 for rc in bashrc zshrc; do
   if [[ -s "$HOME/.$rc" ]] && ! grep -q "$exec_string" "$HOME/.$rc"; then
-    echo >> "$HOME/.$rc"
-    echo "$exec_string" >> "$HOME/.$rc"
+    echo -e "\n$exec_string" >> "$HOME/.$rc"
     echo "== Added SCM Breeze to '~/.$rc'"
   fi
 done
