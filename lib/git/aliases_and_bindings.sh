f2482cf819706532420fc429827d7617fa420da8
Nathan Broadbent <nathan.f77@gmail.com>
2011-11-17 10:05:09 +0800

    Alias 'git' to the 'hub' wrapper, if installed. Wraps the git command with awesome github focussed shortcuts.

--- lib/git/aliases_and_bindings.sh
+++ lib/git/aliases_and_bindings.sh
@@ -7,6 +7,9 @@
 list_aliases() { alias -p | grep "$*" --color=never | sed -e 's/alias //' -e "s/='/::/" -e "s/'//g" | awk -F "::" '{ printf "\033[1;36m%15s  \033[2;37m=>\033[0m  %-8s\n",$1,$2}'; }
 alias git_aliases="list_aliases git"
 
+# Wrap git with the 'hub' github wrapper, if installed
+# https://github.com/defunkt/hub
+if type hub > /dev/null 2>&1; then alias git=hub; fi
 
 _alias $git_alias='git'
 
