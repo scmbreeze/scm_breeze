006ac0da7ceb97f04d3e6ef05d0adec45ef7d2e0
Nathan Broadbent <nathan.f77@gmail.com>
2012-01-31 00:53:11 +0800

    Disable bash command wrapping until issues are fixed with escaped characters (i.e. whitespace in filenames)

--- lib/git/aliases_and_bindings.sh
+++ lib/git/aliases_and_bindings.sh
@@ -172,14 +172,14 @@ fi
 
 # Bash command wrapping
 # (Works fine with RVM's cd() wrapper)
-if [[ "$bash_command_wrapping_enabled" = "true" ]]; then
-  for cmd in vim cat cd rm cp mv ln; do
-    alias $cmd="exec_git_expand_args $cmd"
-  done
-
-  if [[ "$(type ls)" =~ "--color=auto" ]]; then
-    alias ls="exec_git_expand_args ls --color=auto"
-  else
-    alias ls="exec_git_expand_args ls"
-  fi
-fi
+#~ if [[ "$bash_command_wrapping_enabled" = "true" ]]; then
+  #~ for cmd in vim cat cd rm cp mv ln; do
+    #~ alias $cmd="exec_git_expand_args $cmd"
+  #~ done
+#~ 
+  #~ if [[ "$(type ls)" =~ "--color=auto" ]]; then
+    #~ alias ls="exec_git_expand_args ls --color=auto"
+  #~ else
+    #~ alias ls="exec_git_expand_args ls"
+  #~ fi
+#~ fi
