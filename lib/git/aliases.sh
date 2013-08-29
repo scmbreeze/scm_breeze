e8c79f955ca6880ead01706dd40e7c864b1ebff5
Nathan Broadbent <nathan.f77@gmail.com>
2013-08-29 12:04:26 -0700

    Added git stash pop alias (gashp)

--- lib/git/aliases.sh
+++ lib/git/aliases.sh
@@ -126,6 +126,7 @@ if [ "$git_setup_aliases" = "yes" ]; then
   __git_alias "$git_show_summary" "git" 'show' '--summary'
   __git_alias "$git_stash_alias" "git" 'stash'
   __git_alias "$git_stash_apply_alias" "git" 'stash' 'apply'
+  __git_alias "$git_stash_pop_alias" "git" 'stash' 'pop'
   __git_alias "$git_stash_list_alias" "git" 'stash' 'list'
   __git_alias "$git_tag_alias" "git" 'tag'
 
