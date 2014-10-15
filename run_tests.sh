b6fd7ae829e2060a251fbe5cd06c7ab759f92e75
Matthew Rothenberg <mrothenberg@gmail.com>
2014-10-15 14:12:24 -0400

    split tests up by shell for concurrency
    More parallelism = faster tests.  Also better isolation for changes that
    might only break on zsh or bash respectively.
    
    This is defined via env variable, so someone running locally will have
    all tests run sequentially as before.

--- run_tests.sh
+++ run_tests.sh
@@ -3,8 +3,15 @@
 
 failed=false
 
+# allow list of shells to run tests in to be overriden by environment variable
+# if empty or null, use defaults
+if [ -z "$TEST_SHELLS" ]; then
+  TEST_SHELLS="bash zsh"
+fi
+
+echo "== Will run all tests with following shells: ${TEST_SHELLS}"
 for test in $(find test/lib -name *_test.sh); do
-  for shell in bash zsh; do
+  for shell in $TEST_SHELLS; do
     echo "== Running tests with [$shell]: $test"
     $shell $test || failed=true
   done
