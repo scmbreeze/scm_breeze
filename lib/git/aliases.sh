d763faa68c8580dd049379e43e6e9c7ec32ad47f
Josh Hagins <hagins.josh@gmail.com>
2014-10-30 17:22:17 -0400

    Add aliases for submodule update

--- lib/git/aliases.sh
+++ lib/git/aliases.sh
@@ -137,6 +137,8 @@ if [ "$git_setup_aliases" = "yes" ]; then
   __git_alias "$git_stash_pop_alias"                'git' 'stash' 'pop'
   __git_alias "$git_stash_list_alias"               'git' 'stash' 'list'
   __git_alias "$git_tag_alias"                      'git' 'tag'
+  __git_alias "$git_submodule_update_alias"         'git' 'submodule' 'update' '--init'
+  __git_alias "$git_submodule_update_rec_alias"     'git' 'submodule' 'update' '--init' '--recursive'
 
 
   # Compound/complex commands
