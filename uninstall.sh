e95b9a33a108642a5c01ca070963fc53a6dfaf07
Wilhelmina Drengwitz <ghthor@gmail.com>
2017-01-31 15:42:21 -0500

    Fix printf usage

--- uninstall.sh
+++ uninstall.sh
@@ -3,5 +3,5 @@
 # Remove line from bashrc and zshrc if present.
 for rc in bashrc zshrc; do
   sed -i '/scm_breeze/d' "$HOME/.$rc"
-  printf "Removed SCM Breeze from '$HOME/.$rc''%s\n"
+  printf "Removed SCM Breeze from %s\n" "$HOME/.$rc"
 done
