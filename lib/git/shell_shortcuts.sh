scmbreeze/scm_breeze

commit a6eeebfa91e51ec7aae21f65e0df49e461a4ca68

shell_shortcuts.sh: remove unnecessary subshell

lib/git/shell_shortcuts.sh
@@ -23,7 +23,7 @@ if [ "$shell_command_wrapping_enabled" = "true" ] || [ "$bash_command_wrapping_e
     # Define 'whence' for bash, to get the value of an alias
     type whence > /dev/null 2>&1 || function whence() { LC_MESSAGES="C" type "$@" | sed -$SED_REGEX_ARG -e "s/.*is aliased to \`//" -e "s/'$//"; }
     local cmd=''
-    for cmd in $(echo $scmb_wrapped_shell_commands); do
+    for cmd in $scmb_wrapped_shell_commands; do
       if [ "${scmbDebug:-}" = "true" ]; then echo "SCMB: Wrapping $cmd..."; fi
 
       # Special check for 'cd', to make sure SCM Breeze is loaded after RVM

commit cce7e178d23881340ae60fdd0bc14db2961de0d3

Fix parsing of escaped shell command list
A change made in a6eeebf broke parsing of scmb_wrapped_shell_commands.
Rather than iterating over each command in the space-separated list, the
entire block of commands was treated as a single command.

lib/git/shell_shortcuts.sh
@@ -23,7 +23,7 @@ if [ "$shell_command_wrapping_enabled" = "true" ] || [ "$bash_command_wrapping_e
     # Define 'whence' for bash, to get the value of an alias
     type whence > /dev/null 2>&1 || function whence() { LC_MESSAGES="C" type "$@" | sed -$SED_REGEX_ARG -e "s/.*is aliased to \`//" -e "s/'$//"; }
     local cmd=''
-    for cmd in $scmb_wrapped_shell_commands; do
+    for cmd in $(echo $scmb_wrapped_shell_commands); do
       if [ "${scmbDebug:-}" = "true" ]; then echo "SCMB: Wrapping $cmd..."; fi
 
       # Special check for 'cd', to make sure SCM Breeze is loaded after RVM